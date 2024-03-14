dba051c482bb9eda2d4d490a4e18388ea58c1254
njdam <jeandamngaboyimana@gmail.com>
2024-03-14 17:56:35 +0000

    Define variables!

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Defining and annotating `a`, `pi`, `i_understand_annotations`, and
+`school` variables with the specified values!
+"""
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
