83436dd49aed489bf63e28a70e7f76684c454062
njdam <jeandamngaboyimana@gmail.com>
2024-03-18 11:10:04 +0000

    Let's execute multiple coroutines at the same time with async by function name `wait_n`.

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,10 +2,11 @@
 """ Let's execute multiple coroutines at the same time with async """
 
 import asyncio
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Call wait_random with max_delay in n times
     and returns the list of all the delays
@@ -13,5 +14,6 @@ async def wait_n(n: int, max_delay: int) -> list:
     delay_list = []
     for _ in range(n):
         delay_list.append(wait_random(max_delay))
+    delays = await asyncio.gather(*delay_list)  # For awaiting the delays
 
-    return [await delay for delay in delay_list]
+    return sorted(delays)
