fe138d089b55ed5c72f080ea26c493a03e03e221
njdam <jeandamngaboyimana@gmail.com>
2024-03-19 15:09:05 +0000

    Async Comprehensions!

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python3
 """ Async Comprehensions """
 
-import asyncio
 from typing import List
 from importlib import import_module as using
-
-
 async_generator = using('0-async_generator').async_generator
 
 
